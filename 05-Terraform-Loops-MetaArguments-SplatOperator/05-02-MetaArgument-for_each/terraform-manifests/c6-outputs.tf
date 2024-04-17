570ccfb060c95976bd23c92621e685954a36a011
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2024-04-17 18:51:56 +0300

    05-02

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/terraform-manifests/c6-outputs.tf
@@ -0,0 +1,34 @@
+/*
+
+1. For Loop List
+2. For Loop with Map
+3. For Loop with Map Advanced
+4. Legacy Splat Operator (latest) - Returans the list
+5. Latest Generalized Splat Operator - Return the list
+
+*/
+
+
+# EC2 Instance Public IP with TOSET
+
+output "instance_publicip" {
+  description = "EC2 Instance Public IP"
+  #value       = aws_instance.myec2vm[*].public_ip
+  value = toset([for instance in aws_instance.myec2vm : instance.public_ip])
+}
+
+# EC2 Instance Public DNS with TOSET
+
+output "instance_publicdns" {
+  description = "EC2 Instance Public DNS"
+  #value       = aws_instance.myec2vm[*].public_dns
+  value = toset([for instance in aws_instance.myec2vm : instance.public_dns])
+}
+
+# EC2 Instance Public DNS with TOSMAP
+
+output "instance_publicdns2_map" {
+  description = "EC2 Instance Public DNS"
+  value       = tomap({ for az, instance in aws_instance.myec2vm : az => instance.public_dns })
+}
+
