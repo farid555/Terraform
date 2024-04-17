d7329df6b189d0a770118883cdee28e2fadeba26
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2024-04-17 21:08:36 +0300

    05-04

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-04-for_each-with-az-instancetype-check/terraform-manifests/c1-versions.tf
@@ -0,0 +1,16 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    aws = { #Local Names of provider aws
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+}
+
+#Provider Block
+provider "aws" { #Name will be same Local Names aws
+  region = var.aws_region
+}
